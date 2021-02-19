5cc074dfb20875c5fca3c13f0370c013c016e3b9
Andrew Puig <andrew@shethinx.com>
2021-02-19 11:10:47 -0500

    init

---
+++ components/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { color, space, typography } from "styled-system";
+
+const Button = styled.div`
+  ${color};
+  ${space};
+  ${typography};
+  appearance: none;
+  padding: 20px;
+  text-align: center;
+`;
+export default Button;
---
+++ theme.js
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    black: "#000e1a",
+    blue: "#007ce0",
+    navy: "#004175",
+    red: "tomato",
+  },
+  fonts: {
+    serif: "Georgia",
+  },
+  breakpoints: ["720px", "1024px"],
+};
